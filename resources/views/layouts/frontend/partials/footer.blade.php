7040c5ca0d75467f46a9e00d7fc06a3e1fc73930
shailchat <shailchat@gmail.com>
2023-08-07 10:40:19 +0530

    Added auth

--- resources/views/layouts/frontend/partials/footer.blade.php
+++ resources/views/layouts/frontend/partials/footer.blade.php
@@ -1,7 +1,7 @@
 
 <footer class="text-right fixed inset-x-0 bottom-0 p-4">
     <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-right md:justify-between">
-      <span class="text-sm text-gray-500 sm:text-right dark:text-gray-400">© 2023 <a href="https://www.stpi.in/" class="hover:underline">STPI</a>. All Rights Reserved.
+      <span class="text-sm text-gray-500 sm:text-right">© 2023 <a href="https://www.stpi.in/" class="hover:underline">STPI</a>. All Rights Reserved.
     </span>
 
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EmployeeController;
-use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,30 +14,18 @@ use App\Http\Controllers\StudentController;
 |
 */
 
-
-//https://laravel.com/docs/10.x/routing
-Route::view('/', 'index');
-Route::view('/index1', 'index1');
-Route::view('/index2', 'index2');
-
-Route::prefix('employees')->group(function () {
-    Route::get('', [EmployeeController::class, 'getAllEmployees']);
-    Route::post('', [EmployeeController::class, 'saveEmployee']);
-    Route::put('/{id}', [EmployeeController::class, 'updateEmployee']);
-    Route::get('/deleted', [EmployeeController::class, 'getDeletedEmployees']);
-    Route::get('/{id}/restore', [EmployeeController::class, 'restoreEmployee']);
-    Route::delete('/{id}/force', [EmployeeController::class, 'forceDeleteEmployee']);
-    Route::get('/{id}/edit', [EmployeeController::class, 'showEmployeeEditForm']);
-    Route::get('/add', [EmployeeController::class, 'showEmployeeAddForm'])->name('addEmployeeName');
-    Route::get('/{id}', [EmployeeController::class, 'getEmployeeDetails']);
-    Route::delete('/{id}', [EmployeeController::class, 'deleteEmployee']);
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('/students', [StudentController::class, 'getallstudents']);
-Route::post('/students', [StudentController::class, 'savestudents']);
-Route::get('/students/deleted', [StudentController::class, 'getdeletedstudents']);
-Route::get('/students/{id}/restore', [StudentController::class, 'restorestudents']);
-Route::delete('/students/{id}/force', [StudentController::class, 'forcedeletestudents']);
-Route::get('/students/add', [StudentController::class, 'showstudentsaddform'])->name('addstudentname');
-Route::get('/students/{id}', [StudentController::class, 'getstudentdetails']);
-Route::delete('/students/{id}', [StudentController::class, 'deletestudent']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
